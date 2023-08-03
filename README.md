28a936ad67192b588445e2c46516a7df1c40059d
lvmartinez16 <lizethm745@gmail.com>
2023-08-02 21:33:18 -0500

    readme-plus

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
- Este analizador de texto es una herramienta que permite realizar varias operaciones en un texto ingresado por el usuario. A continuación, se detallan las funciones principales: 
- 1. Palabras: Cuenta la cantidad de palabras presentes en el texto. Las palabras estan separadas por espacios
- * Ejemplo si colocas "Hola, soy tu text analizer!" el resultado es: 5 
+ Este analizador de texto es una herramienta que permite realizar varias operaciones en un texto ingresado por el usuario. A continuación, se detallan las funciones principales:
+
+ 1. Palabras: Esta función cuenta la cantidad de palabras presentes en el texto. Las palabras están separadas por espacios.
+ * Ejemplo si colocas "Hola, soy tu text analyzer!" el resultado es: 5 
 
  2. Caracteres: Esta función cuenta la cantidad total de caracteres presentes en el texto, incluyendo letras, números, espacios y signos de puntuación. 
- * Ejemplo "Hola, soy tu text analizer!" el resultado es: 28
+ * Ejemplo "Hola, soy tu text analyzer!" el resultado es: 27
 
- 3. Caracter sin espacio:  Calcula la cantidad de caracteres en el texto, excluyendo los espacios en blanco. 
- * Ejemplo " "Hola, soy tu text analizer!" el resultado es: 21
+ 3. Caracter sin espacio: Esta función cuenta la cantidad de caracteres que sean letras en el texto.
+ * Ejemplo " "Hola, soy tu text analyzer!" el resultado es: 21
 
- 4. Promedio longitud: Cuanta el promedio longitud del texto excluyendo los signos de puntuacion.
- * Ejemplo "Hola, soy tu text analizer!" el resultado es: 4.6
+ 4. Promedio longitud: Esta función cuenta el promedio en longitud del texto incluyendo los signos de puntuacion.
+ * Ejemplo "Hola, soy tu text analyzer!" el resultado es: 4.6
 
- 5. Números: Esta funcion cuenta solo los números presentes en el texto.
- * Ejemplo "Hola, soy tu text analizer! numero1 version 0.1 de la lista 4" el resultado es: 2 
+ 5. Números: Esta función cuenta solo los números presentes en el texto, el separador decimal debe ser punto (.) y no coma (,)
+ * Ejemplo "Hola, soy tu text analyzer! numero1 version 0.1 de la lista 4" el resultado es: 2 
 
- 6. Suma de números: Esta funcion realiza el conteo de numeros enteros y decimales
+ 6. Suma de números: Esta función realiza la suma de los números presentes en el textoc ya sean enteros o decimales
 * Ejemplo "Hola, soy tu text analizer! numero1 version 0.1 de la lista 4" el resultado es: 4.1 
--- src/analyzer.js
+++ src/analyzer.js
@@ -1,15 +1,27 @@
 const Analyzer = {
-  getWordCount: (text) => {//TODO: esta función debe devolver el recuento de palabras que se encuentran en el parámetro textde tipo string.
+  getWordCount: (text) => {//TODO: esta función debe devolver el recuento de palabras que se encuentran en el parámetro text de tipo string.
+    /* 
+        const textoLimpio = text.trim().split(/\s+/);//realiza una serie de text para obtener un arreglo de palabras limpias. trim elimina espacio inicio y final split
+        const conteo = textoLimpio.length;
+        return conteo; */
+    // Verificar si el texto es vacío o solo contiene signos de puntuación
+    // Si el texto está vacío o es nulo, retornar 0
+    if (!text || text.trim() === '') {
+      return 0;
+    }
 
-    const textoLimpio = text.trim().split(/\s+/);//realiza una serie de text para obtener un arreglo de palabras limpias. trim elimina espacio inicio y final split
-    const conteo = textoLimpio.length;
-    return conteo;
-
+    const palabras = text.split(/\s+/).filter(palabra => palabra.match(/[a-zA-Z]+/)); // Expresión regular para encontrar palabras y eliminar signos de puntuación
+    return palabras.length;// Retornar la cantidad de palabras válidas
   },
 
 
   getCharacterCount: (text) => { //TODO: esta función debe devolver el recuento de caracteres que se encuentran en el parámetro textde tipo string.
-    return text.length;
+    /*   return text.length; */
+    // Verificar si el texto es vacío o solo contiene signos de puntuación
+    if (!text.trim() || /^[.,!¡¿?_;:()[\s]+$/.test(text)) {
+      return 0;
+    }
+    return text.length; // Retornar el recuento de caracteres
 
   },
 
@@ -31,7 +43,7 @@ const Analyzer = {
     return longitudPromedioRedondeada;
   },
   getNumberCount: (text) => {  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    
+
     const regex = /(?:\b|\s)\d+(?:\.\d+)?(?!\w)/g; // Utilizar una expresión regular para buscar números con o sin decimales
     const numerosEncontrados = text.match(regex);  // Buscar todos los números en el texto y obtenerlos en un array
     const cantidadNumeros = numerosEncontrados ? numerosEncontrados.length : 0;
@@ -39,7 +51,7 @@ const Analyzer = {
     return cantidadNumeros;
   },
   getNumberSum: (text) => { //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-   
+
     const regex = /(?:\b|\s)\d+(?:\.\d+)?(?!\w)/g;// Utilizar una expresión regular para buscar números con o sin decimales
     const numerosEncontrados = text.match(regex);// Buscar todos los números en el texto y obtenerlos en un array
     let sumaNumeros = 0; // Inicializar la variable para almacenar la suma de los números
